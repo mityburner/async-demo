7be1bcec807ba5e5c37e22d42be35637614db215
mityburner <mityburner@gmail.com>
2015-07-27 15:07:47 +0800

    create whois.js
    whois查询，给定域名查询相关的信息

---
+++ whois.js
@@ -0,0 +1,27 @@
+var net = require('net');
+var async = require('async');
+
+function whois(domain, callback) {
+    var stream = net.createConnection(43, 'whois.nic.guru'),
+        buffer = '';
+
+    stream.addListener('connect', function() {
+        stream.write(domain + '\r\n');
+    });
+
+    stream.addListener('data', function(data) {
+        buffer += data.toString();
+    });
+
+    stream.addListener('error', function(err) {
+        callback(err, null);
+    });
+
+    stream.addListener('end', function() {
+        stream.end();
+        callback(null, buffer.substring(0,30));
+    });
+
+}
+
+exports.whois = whois;
